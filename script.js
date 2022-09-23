8628de71a55742abe050186094d616c86a1e2d40
Norozkhani <madeleine.norozkhanni@gmail.com>
2022-09-23 10:11:17 +0200

    new

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*
+let name = 'Bratz';
+let greeting = 'Hello';
+let a = 10;
+let b = 7;
+
+alert(greeting + ' ' + name + ' ' + (a + b)); 
+let content = document.getElementById ('content')
+
+content.innerHTML = 'BRATZ BRATZ BRATZ';
+content.style.color = 'hotpink';
+content .style.backgroundColor = 'pink';
+*/
